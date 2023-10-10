0f86acba2191347e78e3fb73922824b6045a56f5
Adrian Fernandez <adferdev@gmail.com>
2023-10-11 00:11:44 +0200

    add token storage impl

--- src/services/token.go
+++ src/services/token.go
@@ -4,12 +4,24 @@ import (
 	"errors"
 	"gocker-api/database"
 	"gocker-api/models"
+	"gocker-api/storage"
 )
+
+var tokenStorage storage.Storage = &storage.TokenStorage{}
+
+func GetTokenById(id int) (*models.Token, error) {
+	token, getTokenErr := tokenStorage.Get(id)
+
+	if getTokenErr != nil {
+		return nil, getTokenErr
+	}
+
+	return token.(*models.Token), nil
+}
 
 // Function that gets a token by its value
 func GetTokenByValue(tokenString string) (token models.Token, err error) {
 	database := database.GetInstance().GetDB()
-
 	result := database.Find(&token, "token_value LIKE ?", tokenString)
 
 	if result.RowsAffected == 0 {
@@ -20,9 +32,12 @@ func GetTokenByValue(tokenString string) (token models.Token, err error) {
 }
 
 // Function that saves a token to the database
-func CreateToken(token *models.Token) {
-	database := database.GetInstance().GetDB()
-	database.Create(token)
+func CreateToken(token *models.Token) (*models.Token, error) {
+	if createErr := tokenStorage.Create(token); createErr != nil {
+		return nil, createErr
+	}
+
+	return token, nil
 }
 
 // Function that deletes a token from the database
